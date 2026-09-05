7dbdbeb5f7d8f6ed2eefc6bd3cdf741104684d17
satyamghai <satyamm34201@gmail.com>
2026-09-05 06:49:51 +0000

    Add frontend continuous deployment

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -187,9 +187,9 @@ resource "aws_iam_role_policy_attachment" "eks_service" {
 # EKS Node Group
 ##################
 # Track latest release for the given k8s version
-data "aws_ssm_parameter" "eks_ami_release_version" {
-  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2/recommended/release_version"
-}
+#data "aws_ssm_parameter" "eks_ami_release_version" {
+# name = "/aws/service/eks/optimized-ami/${var.k8s_version}/amazon-linux-2023/x86_64/standard/recommended/release_version"
+#}
 
 resource "aws_eks_node_group" "main" {
   node_group_name = "udacity"
@@ -197,7 +197,7 @@ resource "aws_eks_node_group" "main" {
   version         = aws_eks_cluster.main.version
   node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = [var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id]
-  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
+  ami_type = "AL2_x86_64"
   instance_types  = ["t3.small"]
 
   scaling_config {
@@ -317,8 +317,8 @@ resource "aws_iam_user" "github_action_user" {
 }
 
 resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
+user   = aws_iam_user.github_action_user.name
+policy = data.aws_iam_policy_document.github_policy.json
 }
 
 data "aws_iam_policy_document" "github_policy" {
